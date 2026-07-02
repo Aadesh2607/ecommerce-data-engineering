764f66c65e989fa9e5877f6e5b8d07ece49f3119
Aadesh Gaikwad <aadeshg790@gmail.com>
2026-07-03 03:23:56 +0530

    Try alternate import order to satisfy ruff/isort

--- airflow/dags/ecommerce_pipeline.py
+++ airflow/dags/ecommerce_pipeline.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 
-from airflow import DAG
 from airflow.providers.standard.operators.bash import BashOperator
+from airflow import DAG
 
 default_args = {
     "owner": "Aadesh",
